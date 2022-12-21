a25d7e6117db80353c4e8d47ebcfc79d346efd74
sofoyan gevork <aab81@bk.ru>
2022-12-21 16:31:58 +0300

    Models - WPF MVVM TUTORIAL #1
    https://www.youtube.com/watch?v=fZxZswmC_BY&list=PLA8ZIAm2I03hS41Fy4vFpRw8AdYNBXmNm

--- Reservoom/App.xaml.cs
+++ Reservoom/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Reservoom.Exeptions;
+using Reservoom.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,42 @@ namespace Reservoom
     /// </summary>
     public partial class App : Application
     {
+        //Переопределить при запуске
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Singleton Suites");
+            try
+            {
+                //Бронируем номер
+                hotel.MakeReservation(new Reservation(
+                    //первый этаж третий номер
+                    new RoomID(1, 3),
+                    //имя
+                    "SingletonSean",
+                    //Время вьезда
+                    new DateTime(2000, 1, 1),
+                    //Время выезда
+                    new DateTime(2000, 1, 2)));
+
+                hotel.MakeReservation(new Reservation(
+                    //первый этаж третий номер
+                    new RoomID(1, 3),
+                    //имя
+                    "SingletonSean",
+                    //Время вьезда
+                    new DateTime(2000, 1, 1),
+                    //Время выезда
+                    new DateTime(2000, 1, 4)));
+            }
+            catch(ReservationConflictExeption ex)
+            {
+
+            }
+            
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("SingletonSean");
+
+            base.OnStartup(e);
+        }
     }
 }
